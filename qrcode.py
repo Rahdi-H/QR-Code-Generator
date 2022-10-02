3b7c4700373680956145c2f635045eec489142d4
Rahdi <rhrahdi@gmail.com>
2022-10-02 11:10:15 +0600

    Init

---
+++ qrcode.py
@@ -0,0 +1,17 @@
+import qrcode
+
+def qr_code_generator(text):
+
+    qr = qrcode.QRCode(
+        version = 1,
+        error_correction = qrcode.constants.ERROR_CORRECT_L,
+        box_size = 10,
+        border = 4
+    )
+    qr.add_data(text)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color = 'black', back_color = 'white')
+    img.save('qrcode.png')
+
+url = input('Enter url : ')
+qr_code_generator(url)
